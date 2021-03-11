6edc0cc5d331eaabc72f3dfc3c699d8fc513f02e
BCL <bcl.contract.tester@gmail.com>
2021-03-10 22:26:46 -0800

    completed Ass04, though not sure what the purpose of Calculate private function in calculator.js was

--- js/03-basic-calculator.js
+++ js/03-basic-calculator.js
@@ -1,12 +1,36 @@
 // IMPORT THE MODULE
-import * as calc from "./modules/calculation.js";
+import * as calc from "./modules/calculator.js";
 
 // COLLECT NUMBER 1, NUMBER 2, AND OPERATION FROM THE USER
 
-console.log(calc.add(3, 4));
-console.log(calc.subtract(3, 4));
-console.log(calc.multiply(3, 4));
-console.log(calc.divide(3, 4));
+let num1 = Number(prompt("Enter the first number:"));
+let num2 = Number(prompt("Enter the second number:"));
+
+let operation;
+
+while (true) {
+    operation = String(prompt("Enter 'add', 'subtract', 'multiply', or 'divide':"));
+    if (operation === "add" || operation === "subtract" || operation === "multiply" || operation === "divide") {
+        break;
+    } else {
+        alert("You did not enter 'add', 'subtract', 'multiply', or 'divide'. Try again.");
+    }
+}
 
 // CHECK TO SEE WHAT OPERATION THEY'RE USING
 // CALL THE APPROPRIATE FUNCTION
+
+switch (operation) {
+    case "add":
+        alert(calc.add(num1, num2));
+        break;
+    case "subtract":
+        alert(calc.subtract(num1, num2));
+        break;
+    case "multiply":
+        alert(calc.multiply(num1, num2));
+        break;
+    case "divide":
+        alert(calc.divide(num1, num2));
+}
+
--- js/modules/calculator.js
+++ js/modules/calculator.js
@@ -1,13 +1,22 @@
 // ADD A PRIVATE FUNCTION CALLED CALCULATE
 
+const calculate = () => {
+    // I don't understand what the purpose is of this private function.
+}
+
 // ADD FOUR PUBLIC FUNCTIONS BELOW
 
 // ADD FUNCTION
-
+export const add = (a, b) => a + b;
 // SUBTRACT FUNCTION
-
+export const subtract = (a, b) => a - b;
 // MULTIPLY FUNCTION
-
+export const multiply = (a, b) => a * b;
 // DIVIDE FUNCTION
+export const divide = (a, b) => a / b;
+
+
+
+
 
 // EXPORT THE FOUR PUBLIC FUNCTIONS
